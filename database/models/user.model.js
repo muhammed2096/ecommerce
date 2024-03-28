e5d7d3be5b8c5955bd94f6dd004f6fed58ea5d72
muhammed2096 <muhammedmostafa744@gmail.com>
2024-03-28 22:40:39 +0200

    update

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -1,63 +1,140 @@
+// import mongoose from "mongoose";
+// import bcrypt from "bcrypt"
+
+// const schema = new mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true,
+//         trim:true,
+//     },
+//     email:{
+//         type:String,
+//         required:true,
+//         // unique:true,
+//         trim:true
+//     },
+//     phone:{
+//         type:String,
+//         required:true
+//     },
+//     role:{
+//         type:String,
+//         enums:["admin", "user"],
+//         default:"user"  
+//     },
+//     password:{
+//         type:String,
+//         required:true,
+//     },
+//     isActive:{
+//         type:Boolean,
+//         default:true,
+//     },
+//     isBlocked:{
+//         type:Boolean,
+//         default:false
+//     },
+//     isVerfied:{
+//         type:Boolean,
+//         default:false
+//     },
+//     changePasswordAt:Date,
+//     wishlist:[{type:mongoose.Types.ObjectId, ref:'Product'}],
+//     addresses:[{
+//         street:String,
+//         phone:String,
+//         city:String
+//     }]
+// },{
+//     timestamps:true
+// })
+
+// schema.pre("save", function(){
+//     console.log(this);
+//     if(this.password) this.password = bcrypt.hashSync(this.password, parseInt(process.env.SALT_ROUNDS))
+// })
+
+// schema.pre("findOneAndUpdate", function(){
+//     console.log(this);
+//    if(this._update.password) this._update.password = bcrypt.hashSync(this._update.password, parseInt(process.env.SALT_ROUNDS))
+// })
+
+
+// const userModel = mongoose.model("User", schema);
+// export default userModel
+
+import bcrypt from 'bcrypt'
 import mongoose from "mongoose";
-import bcrypt from "bcrypt"
 
 const schema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
         trim:true,
+        minLength:[3,'name is too short'],
+        maxLength:[20,'name is too long']
     },
     email:{
         type:String,
         required:true,
+        trim:true,
         unique:true
-    },
-    phone:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enums:["admin", "user"],
-        default:"user"  
     },
     password:{
         type:String,
         required:true,
     },
-    isActive:{
-        type:Boolean,
-        default:true,
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user',
+        lowercase : true
     },
     isBlocked:{
         type:Boolean,
         default:false
     },
-    isVerfied:{
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    confirmEmail:{
         type:Boolean,
         default:false
     },
-    changePasswordAt:Date,
-    wishlist:[{type:mongoose.Types.ObjectId, ref:'Product'}],
-    addresses:[{
-        street:String,
+    passwordChangedAt : Date,
+    wishList : [{
+        type:mongoose.Types.ObjectId,
+        ref:'product'
+    }],
+    address : [{
+        streat:String,
         phone:String,
         city:String
-    }]
-},{
-    timestamps:true
-})
+    }],
+    verifyCode: {
+        type: String,
+    },
+    resetCode: String,
+    passwordChangedAT: Date,
+    isverify: {
+        type: Boolean,
+        default: false
+    },
+    confirmEmail: {
+        type: Boolean,
+        default: false
+    }
+},{ timestamps: true })
 
-schema.pre("save", function(){
-    console.log(this);
-    if(this.password) this.password = bcrypt.hashSync(this.password, parseInt(process.env.SALT_ROUNDS))
-})
 
-schema.pre("findOneAndUpdate", function(){
-    console.log(this);
-   if(this._update.password) this._update.password = bcrypt.hashSync(this._update.password, parseInt(process.env.SALT_ROUNDS))
+schema.pre('save',function(){
+    this.password = bcrypt.hashSync(this.password,8)
 })
-
+schema.pre('findOneAndUpdate', function () {
+    if (this._update.password)
+        this._update.password = bcrypt.hashSync(this._update.password, 8)
+})
 
 const userModel = mongoose.model("User", schema);
 export default userModel
--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 
 export const signUpSchema = Joi.object({
-    name:Joi.string().min(3).max(10).trim().required(),
+    name: Joi.string().min(2).max(100).required(),
     email:Joi.string().email().required(),
     password: Joi.string().pattern(/^[A-Z][a-z0-9#@]{8,40}$/).required(),
     rePassword: Joi.valid(Joi.ref('password')).required(),
